f3647ecf1fe55de4fabd682ba3d958e858f2e252
Adam Gausmann <agausmann@fastmail.com>
2021-11-21 19:08:18 -0600

    Add Oscillator

---
+++ examples/oscillator.rs
@@ -0,0 +1,9 @@
+use grounded::Oscillator;
+
+fn main() {
+    let mut osc = Oscillator::new(48000.0, 1200.0);
+
+    for _ in 0..100 {
+        println!("{}", osc.next().unwrap());
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,28 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+use std::f64::consts::TAU;
+
+pub struct Oscillator {
+    sample_rate: f64,
+    frequency: f64,
+    phase: f64,
+}
+
+impl Oscillator {
+    pub fn new(sample_rate: f64, frequency: f64) -> Self {
+        Self {
+            sample_rate,
+            frequency,
+            phase: 0.0,
+        }
+    }
+}
+
+impl Iterator for Oscillator {
+    type Item = f64;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let sample = self.phase.sin();
+        self.phase += TAU * self.frequency / self.sample_rate;
+        self.phase %= TAU;
+        Some(sample)
     }
 }
